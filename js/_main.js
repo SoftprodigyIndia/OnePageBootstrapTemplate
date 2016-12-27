c50721e81ab9e6d97a8905ae141e167bc7c2d12a
SoftProdigy Official <resources@softprodigy.com>
2016-12-27 13:35:51 +0530

    Initial code commit

---
+++ js/_main.js
@@ -0,0 +1,52 @@
+ /**
+ * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
+ * Want to see more similar demos and tutorials?
+ * Help by spreading the word about Ihatetomatoes blog.
+ * Facebook - https://www.facebook.com/ihatetomatoesblog
+ * Twitter - https://twitter.com/ihatetomatoes
+ * Google+ - https://plus.google.com/u/0/109859280204979591787/about
+ * Article URL: http://ihatetomatoes.net/simple-parallax-scrolling-tutorial/
+ */
+
+( function( $ ) {
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
+    //FadeIn all sections   
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+		
+		// Init Skrollr
+		
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 1100) {
+			winH = 1100;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    
+	    
+	}
+		
+} )( jQuery );
